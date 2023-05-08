299e65ea69aac29fea1da4f428c2385d206fd51c
Kei <ashkarlsefni@gmail.com>
2023-05-08 20:02:15 +0100

    first version

--- src/main/java/com/example/tppoo/Creneaux.java
+++ src/main/java/com/example/tppoo/Creneaux.java
@@ -1,10 +1,20 @@
 package com.example.tppoo;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Creneaux implements Decomposable{
 
+ private LocalDate jour;
+ private LocalTime HeureDebut;
+ private LocalTime HeureFin;
+ private static int dureeMin;
 
 
 
+    public boolean verifDureeMin(){
+        return false;
+    }
 
 
     @Override
